1dab72ec1e56bb6361086a30b945fd30ca46fc43
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-02-08 15:18:56 -0500

    spatial subsetting is PGH-specific

--- R/osm.R
+++ R/osm.R
@@ -9,24 +9,3 @@ read_osm_response <- function(raw_response) {
   write_lines(raw_response, path = tfile)
   get_osm(complete_file(), source = osmsource_file(tfile))
 }
-
-# Convert OSM nodes to sf so that we can determine which ones are withi the PGH
-# administrative boundary
-osm_nodes_to_sf <- function(osm) {
-  nodes_sf <- cbind(osm$nodes$attrs$lon, osm$nodes$attrs$lat) %>%
-    st_multipoint() %>%
-    st_sfc(crs = 4326) %>%
-    st_cast("POINT")
-
-  res <- select(osm$nodes$attrs, id)
-  st_geometry(res) <- nodes_sf
-  res
-}
-
-nodes_within_boundaries <- function(nodes, boundaries) {
-  res <- st_intersects(nodes, boundaries, sparse = FALSE) %>%
-    apply(1, any)
-
-  nodes$id[which(res)]
-}
-
