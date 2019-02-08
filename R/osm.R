d1712193f9414ad8c4f676432ba587b109f05393
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-02-08 16:03:09 -0500

    bounding box retrieval test

--- R/osm.R
+++ R/osm.R
@@ -1,11 +1,27 @@
 # Data utilities ----
 
-get_osm_bbox <- function(xlim, ylim) {
-  content(GET(str_glue("https://overpass-api.de/api/map?bbox={xlim[1]},{ylim[1]},{xlim[2]},{ylim[2]}")), as = "text", encoding = "UTF-8")
-}
-
-read_osm_response <- function(raw_response) {
+#' Download OSM data from a specified lon/lat bounding box
+#'
+#' Downloads OSM data from an [Overpass API server](http://overpass-api.de) and reads it into an [`osmar`] object.
+#'
+#' @seealso [`osmar::osmsource_api`] which iplements the same function but has stricter controls on the size that can be downloaded.
+#'
+#' @param xmin, xmax, ymin, ymax Numeric. Minimum and maximum latitude and longitude
+#' @param server Character. Hostname of an [Overpass API server](http://overpass-api.de) Defaults to <https://overpass-api.de> although you may specify a different hostname.
+#'
+#' @return An [`osmar`] object.
+#' @export
+get_osm_bbox <- function(xmin, xmax, ymin, ymax, server = "https://overpass-api.de") {
   tfile <- tempfile()
-  write_lines(raw_response, path = tfile)
-  get_osm(complete_file(), source = osmsource_file(tfile))
+  message("Downloading...", appendLF = FALSE)
+  res <- httr::GET(glue::glue("{server}/api/map?bbox={xmin},{ymin},{xmax},{ymax}"), httr::write_disk(tfile), httr::progress())
+  message("complete!")
+
+  message("Reading XML...", appendLF = FALSE)
+  osm_res <- osmar::get_osm(osmar::complete_file(), source = osmar::osmsource_file(tfile))
+  message("complete!")
+
+  unlink(tfile)
+
+  osm_res
 }
---
+++ tests/testthat/test-osm.R
@@ -0,0 +1,11 @@
+context("test-osm")
+
+test_that("osm bbox returns a correct osmar object", {
+  xmin <- -73.11673
+  xmax <- -73.10871
+  ymin <- 42.69575
+  ymax <- 42.70097
+
+  expect_message(res <- get_osm_bbox(xmin, xmax, ymin, ymax), regexp = "complete")
+  expect_is(res, "osmar")
+})
