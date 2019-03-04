c3fa4651423e09aaaa7ee5858bd3685a85cdeb54
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-03-04 16:43:09 -0500

    test suite for sf

--- tests/testthat/test-sf.R
+++ tests/testthat/test-sf.R
@@ -1,5 +1,21 @@
 context("test-sf")
 
-test_that("graph_to_edges returns sf with linestrings", {
+test_that("graph to sf", {
+  suppressMessages({
+    boston_graph <- konigsberg_graph(boston)
+  })
 
+  boston_sf <- graph_to_sf(boston_graph)
+  expect_is(boston_sf, "sf_graph")
+
+  boston_points <- boston_sf$vertices
+  boston_linestrings <- boston_sf$edges
+
+  expect_is(boston_points, "sf")
+  expect_true(all(st_geometry_type(boston_points) == "POINT"))
+  expect_equal(nrow(boston_points), vcount(boston_graph))
+
+  expect_is(boston_linestrings, "sf")
+  expect_true(all(st_geometry_type(boston_linestrings) == "LINESTRING"))
+  expect_equal(nrow(boston_linestrings), ecount(boston_graph))
 })
